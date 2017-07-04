bfd4ffe26f309070c94e7bb417681102ff662fe4
Amit cohen <amitcoh@bynet.co.il>
2017-07-05 02:39:52 +0300

    edit dependencies

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='flask',
 	  author='Prahlad Yeri',
 	  author_email='prahladyeri@yahoo.com',
 	  url='http://www.python.org/sigs/distutils-sig/',
-	  install_requires=['Flask==0.10.1','SQLAlchemy==0.9.8'],
+	  install_requires=['Flask>=0.10.1','SQLAlchemy>=0.9.8'],
 	 )
