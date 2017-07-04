c1c21816268e1dfa30b871d06aa2b2f9658a7e91
Amit cohen <amitcoh@bynet.co.il>
2017-07-05 02:37:10 +0300

    application

---
+++ wsgi/Flask.py
@@ -0,0 +1,12 @@
+import flask
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.run(debug=True)
