f2462a975dbafa69d708ed824f1b46b72d39772c
Amit cohen <amitcoh@bynet.co.il>
2017-07-09 01:27:39 +0300

    edit wsgi

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,10 @@
 # line, it's possible required libraries won't be in your searchable path
 #
 
-from app import app as application
+from app import app
+
+if __name__ == "__main__":
+    app.run()
 
 #
 # Below for testing only
